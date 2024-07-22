bc2908cb614f58a4e49094cd4c4ac069a4151cfc
Gonçalo Duarte <goncalopduarte23@gmail.com>
2024-07-22 15:45:48 +0100

    day1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,11 @@
 class Question {
-    constructor (text, choices, answer, difficulty) {
-        this.text = text;
-        this.choices = choices;
-        this.answer = answer;
-        this.difficulty = difficulty
-    }
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+  shuffleChoices() {
+    this.choices.sort((a, b) => 0.5 - Math.random());
+  }
+}
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,33 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 4. shuffleQuestions()
+  shuffleQuestions() {
+    this.questions.sort((a, b) => 0.5 - Math.random());
+  }
 
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    if (answer === this.questions[this.currentQuestionIndex].answer) {
+      this.correctAnswers++;
+    }
+  }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
