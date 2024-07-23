82d57d5120114f219fb20f4d7a0973a73e9272df
Gonçalo Duarte <goncalopduarte23@gmail.com>
2024-07-23 15:29:28 +0100

    day2

--- src/quiz.js
+++ src/quiz.js
@@ -30,4 +30,21 @@ class Quiz {
       return true;
     }
   }
+
+  filterQuestionsByDifficulty(difficulty) {
+    if (typeof difficulty === "number" && difficulty >= 1 && difficulty <= 3) {
+      return (this.questions = this.questions.filter(
+        (questions) => questions.difficulty == difficulty
+      ));
+    }
+  }
+
+  averageDifficulty() {
+    const averageDif =
+      this.questions.reduce(
+        (acc, currentValue) => acc + currentValue.difficulty,
+        0
+      ) / this.questions.length;
+    return averageDif;
+  }
 }
